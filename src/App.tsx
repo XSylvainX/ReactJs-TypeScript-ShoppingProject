273cff1ee98c68b45701ba3cc106ebc5ac3a4430
Sylvain_C <scarlier@cutii.io>
2022-02-03 17:56:56 +0100

    update files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+
+// Material Components 
+
+import Drawer from '@mui/material';
+import LinearProgress from '@mui/material';
+import Grid from '@mui/material';
+import AddShoppingCart from '@mui/icons-material';
+import Badge from '@mui/icons-material';
+
+// Styles 
+
+import { Wrapper } from './App.styles';
+
+
 
 
 const App = () => {
