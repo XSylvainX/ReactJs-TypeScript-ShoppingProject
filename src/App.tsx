83ad1fe4438ffb16c970172d567023fbe4e9a6d0
Sylvain_C <scarlier@cutii.io>
2022-02-03 16:37:02 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+<<<<<<< HEAD
+=======
+
+>>>>>>> e85a451... first commit
 
 
 const App = () => {
   return (
     <div className="App">
+<<<<<<< HEAD
       Start
+=======
+      start
+>>>>>>> e85a451... first commit
     </div>
   );
 }
