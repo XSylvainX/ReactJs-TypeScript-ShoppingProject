1aff2435f8186e42652b46dd58cecfd70d3ed47f
Sylvain_C <scarlier@cutii.io>
2022-02-11 14:19:21 +0100

    update CartItem

--- src/Cartitem/CartItem.tsx
+++ src/Cartitem/CartItem.tsx
@@ -19,8 +19,8 @@ const CartItem: React.FunctionComponent<Props> = ({ item, addToCart, removeFromC
         <div>
             <h3>{item.title}</h3>
             <div className='information'>
-                <p>Price: ${item.price}</p>
-                <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+                <p>Price: €{item.price}</p>
+                <p>Total: €{(item.amount * item.price).toFixed(2)}</p>
             </div>
             <div className='buttons'>
                 <Button
